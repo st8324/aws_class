bbea54128ab17600bfeb3fae4631a086e7a71fda
st8324 <stajun@naver.com>
2025-12-02 12:19:48 +0900

    자바 02일차 : 과제 풀이

--- java_jik/src/day01/Ex15_HomeWork.java
+++ java_jik/src/day01/Ex15_HomeWork.java
@@ -1,5 +1,7 @@
 package day01;
 
+import java.util.Scanner;
+
 public class Ex15_HomeWork {
 
 	public static void main(String[] args) {
@@ -13,7 +15,36 @@ public class Ex15_HomeWork {
 		 * F : -9 ~ 59
 		 * 잘못된 성적 : -9미만, 110이상 
 		 * */
-
+		Scanner scan = new Scanner(System.in);
+		
+		//입력 안내문구
+		System.out.print("성적 입력 : ");
+		//성적을 입력
+		int score = scan.nextInt();
+		
+		//성적에 따라 학점을 출력
+		// 1~9를 10으로 나누면 몫 : 0,
+		// 10~19를 10으로 나누면 몫 : 1
+		switch(score / 10) {
+		case 9, 10:
+			System.out.println(score + "점은 A");
+			break;
+		case 8:
+			System.out.println(score + "점은 B");
+			break;
+		case 7:
+			System.out.println(score + "점은 C");
+			break;
+		case 6:
+			System.out.println(score + "점은 D");
+			break;
+		case 0,1,2,3,4,5:
+			System.out.println(score + "점은 F");
+			break;
+		default:
+			System.out.println(score+"점은 잘못된 점수");
+			break;
+		}
 	}
 
 }

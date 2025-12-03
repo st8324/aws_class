3b30d6c6e2f46532f55d1a83f6a535599245c02b
st8324 <stajun@naver.com>
2025-12-03 14:19:11 +0900

    자바 03일차 : 메서드

---
+++ java_jik/src/day03/Ex09_MethodTest.java
@@ -0,0 +1,12 @@
+package day03;
+
+public class Ex09_MethodTest {
+
+	public static void main(String[] args) {
+		/* 두 정수와 산술 연산자가 주어지면 연산 결과를 알려주는 메서드를 만들고,
+		 * 만들어진 메서드를 호출해서 테스트해보세요.
+		 * */
+
+	}
+
+}

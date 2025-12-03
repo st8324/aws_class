0362d472515966532ecb98ef4566c1176c07ec82
st8324 <stajun@naver.com>
2025-12-03 09:24:49 +0900

    자바 02일차 : 과제 풀이

--- java_jik/src/day02/HW01_LCM.java
+++ java_jik/src/day02/HW01_LCM.java
@@ -13,17 +13,20 @@ public class HW01_LCM {
 		 * 6과 8의 최소 공배수 : 24
 		 * */
 		
-		
-		
-		
-		
-		
-		
 		/* 반복횟수 : i는 1부터 1씩 증가(까지가 없음)
 		 * 규칙성 : i가 num1과 num2의 공배수이면 i를 출력 후 반복문 종료 
 		 * =>i가 num1의 배수이고, i가 num2의 배수이면 i를 출력 후 반복문 종료
 		 * =>i를 num1로 나누었을 때 나머지가 0과 같고 i를 num2로 나누었을 때 나머지가 0과 같다면 ~
 		 * */
+		int num1 = 6, num2 = 7;
+		int i = 1;
+		while(true) {
+			if(i % num1 == 0 && i % num2 == 0) {
+				System.out.println(num1 + "과 " + num2 + "의 최소 공배수 : " + i);
+				break;
+			}
+			i++;
+		}
 	}
 
 }

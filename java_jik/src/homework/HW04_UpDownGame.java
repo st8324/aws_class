483147fe1ca168728fdf25f2231701ed6570d422
st8324 <stajun@naver.com>
2025-12-03 18:20:26 +0900

    자바 03일차 : 과제

---
+++ java_jik/src/homework/HW04_UpDownGame.java
@@ -0,0 +1,21 @@
+package homework;
+
+public class HW04_UpDownGame {
+
+	public static void main(String[] args) {
+
+		/* 1~100사이의 랜덤한 수를 생성해서 맞추는 게임
+		 * 랜덤한 수 : 33//안보여야 함
+		 * 정수입력 : 50
+		 * DOWN
+		 * 정수입력 : 25
+		 * UP
+		 * 정수입력 : 30
+		 * UP
+		 * 정수입력 : 33
+		 * 정답입니다. 
+		 * */
+
+	}
+
+}

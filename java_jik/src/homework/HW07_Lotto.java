aa9996f0ed3c9c847ced0561a9a427284a752c16
st8324 <stajun@naver.com>
2025-12-04 17:22:54 +0900

    자바 04일차 : 과제

---
+++ java_jik/src/homework/HW07_Lotto.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class HW07_Lotto {
+
+	public static void main(String[] args) {
+		/* 1~45사이의 랜덤한 수 6개를 생성하여 배열에 저장하고 출력하세요.
+		 * 중복 X
+		 * */
+		
+		int [] lotto = new int[6];
+		
+		//테스트 할 때는 1~8로 테스트하여 중복되지 않는지 확인 후 이상 없으면 1~45로 변경
+	}
+
+}

7c312455f21e91cb31dccc68cf17d42531338203
st8324 <stajun@naver.com>
2025-12-04 15:23:02 +0900

    자바 04일차 : 과제

---
+++ java_jik/src/homework/HW05_Reverse.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class HW05_Reverse {
+
+	public static void main(String[] args) {
+		/* 4자리 정수를 입력받아 거꾸로 출력하는 코드를 작성하세요.
+		 * 예시
+		 * 입력 : 1234
+		 * 결과 : 4321
+		 * 예시
+		 * 입력 : 2000
+		 * 결과 : 0002
+		 * */
+
+	}
+
+}
---
+++ java_jik/src/homework/HW06_Alphabet.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class HW06_Alphabet {
+
+	public static void main(String[] args) {
+		/* 다음 결과가 출력 되도록 코드를 작성하세요. 
+		 * 참고 예제 : day02.Ex02_ForTest에서 a부터 z까지 한줄로 출력 예제
+		 * a
+		 * ab
+		 * abc
+		 * abcd
+		 * abcde
+		 * ...
+		 * abcdefghijk...xyz
+		 * */
+	}
+
+}

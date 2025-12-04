1b0aaa8074d190f7147386880aa28d6409f65939
st8324 <stajun@naver.com>
2025-12-04 14:01:03 +0900

    자바 04일차 : 복습

--- java_jik/src/day04/Ex03_ConditionalStatement.java
+++ java_jik/src/day04/Ex03_ConditionalStatement.java
@@ -74,6 +74,58 @@ public class Ex03_ConditionalStatement {
 		else {
 			System.out.println("졸업 실패");
 		}
+		
+		/* switch문
+		 * - 언제 사용?
+		 *   - if문에서 각 조건문이 변수 == 값형태로 되어 있는데 변수가 고정인 경우 사용
+		 * - 왜 사용? 
+		 *   - if문보다 구조가 간결  
+		 * - 문법
+		 * 	switch(변수){
+		 * 	case 값1:	if(변수 == 값1)
+		 * 		실행문1;
+		 * 		break;
+		 * 	case 값2:	else if(변수 == 값2)
+		 * 		실행문2;
+		 * 		break;
+		 * 	default:	else
+		 * 		실행문3;
+		 * 	}
+		 * - 주의사항
+		 *   - 값에는 상수(final로 선언한 값)와 리터럴(1,2,3,'a')만 올 수 있음
+		 *   - 값은 문자(''), 정수, 문자열("")만 가능
+		 *   - case에 break가 없으면 실행문 실행 후 다음 case에 실행문으로 이동
+		 *     => case에서는 break를 만나야 switch문을 빠져 나감
+		 *     => 실행문이 같은 경우 break를 생략해서 하나의 실행문으로 여러 case를 관리할 수있다
+		 *     => 실행문이 같아서 case를 함께 관리할 때 case 값1, 값2, ...로 줄여쓸 수 있음 
+		 *     
+		 *        
+		 * */
+		
+		//문자가 산술연산자(+, -, *, /, %)이면 산술연산자입니다라고 출력하는 예제
+		char ch = '+';
+		//변수 == 값
+		switch(ch) {
+		
+		case '+', '-', '*', '/', '%':
+		/*
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+		*/
+			System.out.println(ch + "는 산술 연산자입니다.");
+			break;
+		default:
+			System.out.println(ch + "는 산술 연산자가 아닙니다.");
+		}
 	}
 
 }
+
+
+
+
+
+

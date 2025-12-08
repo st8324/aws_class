252a7c7509808d994c480688babdf444dd1d3721
st8324 <stajun@naver.com>
2025-12-08 10:20:41 +0900

    자바 06일차 : Object 클래스

--- java_jik/src/day06/Ex01_Object.java
+++ java_jik/src/day06/Ex01_Object.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Objects;
+
 public class Ex01_Object{
 
 	public static void main(String[] args) {
@@ -11,11 +13,17 @@ public class Ex01_Object{
 		 * - 객체를 비교할 때 사용하는 메서드
 		 * - Object.equals는 주소를 비교
 		 *   => 메서드 오버라이딩(재정의)해서 필드를 비교하여 같다와 같지 않다를 판별
+		 *   
+		 * toString() p.363
+		 * - 객체의 필드(멤버 변수)를 이용하여 문자열을 만들어 반환하는 메서드
 		 */
 
 		Car c1 = new Car("현대", "아반떼", 2022);
 		Car c2 = new Car("현대", "아반떼", 2022);
 		System.out.println("c1 == c2 : " + (c1 == c2));
+		System.out.println("c1.equals(c2) : " + c1.equals(c2));
+		System.out.println("c1 : " + c1);
+		System.out.println("c2 : " + c2);
 	}
 }
 
@@ -36,6 +44,34 @@ class Car{
 		this.carName = carName;
 		this.year = year;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		//주소가 같은지 확인 => 같은 객체를 공유하는지 확인
+		if (this == obj)
+			return true;
+		//비교 대상이 만들어지지 않으면
+		if (obj == null)
+			return false;
+		//두 객체의 클래스가 다르면
+		if (getClass() != obj.getClass())
+			return false;
+		//두 객체의 클래스가 달라도 비교 가능한 경우 : 상속 관계인 두 클래스로 형변환이 가능해야 함
+		
+		//비교 대상이 같은 클래스이기 때문에 클래스 형변환을 함
+		Car other = (Car) obj;
+		//내가 원하는 값을 이용하여 같다 다르다를 판별 
+		return Objects.equals(carName, other.carName) && year == other.year;
+		//차명이 있고, 차명과 비교대상 차명이 같고, 연식이 같으면 참 아니면 거짓
+		//return carName != null &&  carName.equals(other.carName) && year == other.year;
+	}
+
+	@Override
+	public String toString() {
+		return "[회사 : " + company + ", 차명 : " + carName 
+				+ ", 연식 : " + year + "]";
+	}
+	
 	
 }
 

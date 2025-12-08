be6fa4f49d160492f15fe35df837b4e6b5f0de09
st8324 <stajun@naver.com>
2025-12-08 12:03:44 +0900

    자바 06일차 : String 클래스

--- java_jik/src/day06/Ex02_String.java
+++ java_jik/src/day06/Ex02_String.java
@@ -17,14 +17,34 @@ public class Ex02_String {
 		 * 
 		 * String 클래스 메서드
 		 * - indexOf(문자열) : 문자열이 있으면 시작 번지를 알려줌
-		 * - equals(문자열) : 두 문자열이 같인즈 비교하여 true/false를 반환
-		 * - contains(문자열) : 문자열이 있으면 true, 없으면 fasle
+		 * - equals(문자열) : 두 문자열이 같으면 비교하여 true/false를 반환
+		 * - contains(문자열) : 문자열이 있으면 true, 없으면 false
 		 * - lenght() : 문자열의 길이
 		 * - substring(시작위치) : 시작위치부터 끝까지 문자열 반환
 		 * - trim() : 첫글자 앞 공백 제거, 마지막 글자 뒤 공백 제거
 		 * - split(구분자) : 구분자를 기준으로 문자열을 추출하여 문자열 배열을 반환
 		 */
-
+		String str1 = "Hello world";
+		String str2 = "world";
+		String str3 = "\n\t\t\t안녕 하세요\n\n\n";
+		String str4 = "사과,배,바나나,오렌지";
+		
+		int index = str1.indexOf(str2);
+		System.out.println("str1에 str2가 몇번지부터 시작됩니까? " + index);
+		System.out.println("str1에 str2가 있습니까? " + str1.contains(str2));
+		
+		System.out.println("str1이 str2와 같습니까? " + str1.equals(str2));
+		
+		System.out.println("str1의 길이는? " + str1.length());
+		
+		System.out.println("str1의 2번지부터 부분문자열은? " + str1.substring(2));
+		
+		System.out.println("str3.trim() : " + str3.trim());
+		
+		String fruits [] = str4.split(",");
+		for(String fruit : fruits) {
+			System.out.println(fruit);
+		}
 	}
 
 }

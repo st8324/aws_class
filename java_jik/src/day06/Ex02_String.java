dc610e5de24a9fc552602b1003e7824f1acccde7
st8324 <stajun@naver.com>
2025-12-08 10:20:48 +0900

    자바 06일차 : String 클래스

---
+++ java_jik/src/day06/Ex02_String.java
@@ -0,0 +1,30 @@
+package day06;
+
+public class Ex02_String {
+
+	public static void main(String[] args) {
+		/* String 클래스 p.377
+		 * - 문자열을 관리하는 클래스
+		 * - final char[]로 문자열을 관리
+		 * - 문자열이 변경되면 기존 문자열에서 변경되는게 아니라 새로운 문자열을 만들어서 연결
+		 *   예시
+		 *   String str = "abc";
+		 *   str = str + "d"; //abc 뒤에 d를 이어 붙이는게 아니라 abcd라는 문자열을 새로 만들어 연결
+		 * 
+		 * StringBuffer(쓰레드 지원)와 StringBuilder p.380
+		 * - char[]로 문자열을 관리
+		 * - 둘의 차이는 쓰레드 지원여부
+		 * 
+		 * String 클래스 메서드
+		 * - indexOf(문자열) : 문자열이 있으면 시작 번지를 알려줌
+		 * - equals(문자열) : 두 문자열이 같인즈 비교하여 true/false를 반환
+		 * - contains(문자열) : 문자열이 있으면 true, 없으면 fasle
+		 * - lenght() : 문자열의 길이
+		 * - substring(시작위치) : 시작위치부터 끝까지 문자열 반환
+		 * - trim() : 첫글자 앞 공백 제거, 마지막 글자 뒤 공백 제거
+		 * - split(구분자) : 구분자를 기준으로 문자열을 추출하여 문자열 배열을 반환
+		 */
+
+	}
+
+}

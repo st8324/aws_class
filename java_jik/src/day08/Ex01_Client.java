d4114a1b8b55ba5beb928e98de4cf8d1c90d8ed3
st8324 <stajun@naver.com>
2025-12-10 10:26:43 +0900

    자바 08일차 : 네트워크

--- java_jik/src/day08/Ex01_Client.java
+++ java_jik/src/day08/Ex01_Client.java
@@ -1,6 +1,9 @@
 package day08;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Ex01_Client {
 
@@ -11,12 +14,37 @@ public class Ex01_Client {
 		final int PORT = Ex01_Server.PORT;
 		
 		//2. IP주소와 port를 이용하여 연결 요청후 성공하면 소켓 객체를 생성
-		try(Socket socket = new Socket(ip, PORT)){
+		try{
+			Socket socket = new Socket(ip, PORT);
 			System.out.println("[연결 성공!]");
 			
+			//전송하는 스레드
+			Thread t1 = new Thread(()->{
+				System.out.println("[서버로 전송할 메세지]");
+				
+				Scanner scan = new Scanner(System.in);
+				try {
+					ObjectOutputStream oos
+						= new ObjectOutputStream(socket.getOutputStream());
+					//메세지를 여러번 전송하기 위한 무한루프
+					while(true) {
+						//메세지 콘솔에서 입력
+						System.out.print("입력 : ");
+						String msg = scan.nextLine();
+						//서버로 전송
+						oos.writeUTF(msg);
+						oos.flush();
+					}			
+				} catch (IOException e) {
+					System.err.println("예외 발생");
+				}
+				
+			});
+			t1.start();
 		}catch(Exception e) {
 			System.err.println("예외 발생");
 		}
+		System.out.println("[클라이언트 종료(메인스레드 종료)]");
 	}
 
 }
--- java_jik/src/day08/Ex01_Server.java
+++ java_jik/src/day08/Ex01_Server.java
@@ -1,5 +1,7 @@
 package day08;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,7 +14,8 @@ public class Ex01_Server {
 		
 		
 		//2. port를 이용하여 서버소켓 객체 생성
-		try(ServerSocket serverSocket = new ServerSocket(PORT)){
+		try{
+			ServerSocket serverSocket = new ServerSocket(PORT);
 			
 			System.out.println("[연결 대기 중...]");
 			
@@ -21,11 +24,33 @@ public class Ex01_Server {
 			
 			System.out.println("[연결 성공!]");
 			
+			//클라이언트가 전송하면 받아서 콘솔에 출력 하는 쓰레드
+			Thread t1 = new Thread(()->{
+				System.out.println("[클라이언트가 전송한 메시지]");
+				try {
+					//클라이언트가 보낸 값을 받기 위한 입력 스트림객체 생성
+					ObjectInputStream ois 
+						= new ObjectInputStream(socket.getInputStream());
+					//메세지를 여러번 입력할수 있게 무한 루프
+					while(true) {
+						String msg = ois.readUTF();
+						System.out.println("클라이언트 : " + msg);
+						if(msg.equals("EXIT")) {
+							System.out.println("[클라이언트가 전송을 종료했습니다.]");
+							break;
+						}
+					}
+				} catch (IOException e) {
+					System.err.println("예외가 발생했습니다.");
+				}
+				System.out.println("[수신 스레드 종료]");
+			});
+			t1.start();
 			
 		}catch(Exception e) {
 			System.err.println("예외가 발생했습니다.");
 		}
-
+		System.out.println("[서버 종료(메인 스레드 종료)]");
 	}
 
 }

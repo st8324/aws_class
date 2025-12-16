9d6ca35fa5a1365afce99028bff7ccc95c96c036
st8324 <stajun@naver.com>
2025-12-16 16:51:23 +0900

    DB 03일차 : 프로시저

--- db/09_프로시저.sql
+++ db/09_프로시저.sql
@@ -19,7 +19,7 @@ SHOW CREATE PROCEDURE FILM_IN_STOCK;
 # DROP PROCEDURE IF EXISTS 프로시저명;
 
 /*
-DELIMITER 기호
+DELIMITER 기호 # 프로시저를 정의하는 동안에 문장의 끝이 ;이 안되도록 변경 
 CREATE PROCEDURE 프로시저명( 
 	[IN | OUT | INOUT 변수명 타입,]
 )
@@ -27,7 +27,107 @@ BEGIN
 	프로시저 구현;
 END 기호 
 DELIMITER ;
+
+DELIMITER 
+- 문장(쿼리)의 끝을 나타내는 기호 
+- 기본은 ; 
+- DELIMITER를 이용하여 문장의 끝 기호를 변경할 수 있음 
+
+IN 
+- 프로시저 호출 시 필요한 정보가 있을 때 사용 
+OUT
+- 프로시저가 끝날 때 알려줄 정보가 있을 때 사용
+INOUT
+- 프로시저가 호출 시 필요한 정보가 끝날 때는 알려줄 정보로 바뀔 때 사용 
 */
+
+# 정의한 프로시저 호출
+# CALL 프로시저명(값|변수들);
+
+# 프로시저 삭제 
+DROP PROCEDURE IF EXISTS CURRENT_TIME_NOW;
+# 현재시간의 년, 월, 일을 조회하는 프로시저를 생성 
+DELIMITER $$
+CREATE PROCEDURE CURRENT_TIME_NOW()
+BEGIN
+	SELECT NOW() 현재시간;
+END $$
+DELIMITER ;
+
+CALL CURRENT_TIME_NOW();
+
+DROP PROCEDURE IF EXISTS LPAD_NUMBER;
+DELIMITER $$
+CREATE PROCEDURE LPAD_NUMBER(
+	IN NUM INT 
+)
+BEGIN
+	SELECT LPAD(NUM, 3, '0') LPAD_NUMBER;
+END $$
+DELIMITER ;
+
+CALL LPAD_NUMBER(1);
+# 변수 선언
+# SET @변수명 = 값;
+SET @NUM = 20;
+CALL LPAD_NUMBER(@NUM);
+
+DROP PROCEDURE IF EXISTS PLUS;
+DELIMITER $$
+CREATE PROCEDURE PLUS(
+	IN NUM INT,
+    OUT RES INT
+)
+BEGIN
+	SET RES = NUM + 1;
+END $$
+DELIMITER ;
+
+CALL PLUS(1, @NUM);
+SELECT @NUM;
+
+USE SHOPPINGMALL2;
+DROP PROCEDURE IF EXISTS BUY_CART;
+DELIMITER $$
+CREATE PROCEDURE BUY_CART(
+    IN CART_NUM INT,
+    IN ADDRESS VARCHAR(100)
+)
+BEGIN
+	# BEGIN과 END 사이에서 사용하는 변수는 DECLARE로 선언하고, 시작 위치에 모아 놓아야 함
+	DECLARE _PRICE INT;
+    DECLARE _AMOUNT INT;
+    DECLARE _CODE CHAR(6);
+    DECLARE _ID VARCHAR(13);
+    
+    SET _AMOUNT = (SELECT AMOUNT FROM CART WHERE NUM = CART_NUM);
+    SET _CODE = (SELECT CODE FROM CART WHERE NUM = CART_NUM);
+    SET _ID = (SELECT ID FROM CART WHERE NUM = CART_NUM);
+    SET _PRICE = (SELECT PRICE FROM PRODUCT WHERE CODE = _CODE);
+    
+    # BUY 테이블에 구매 내역 추가 
+    INSERT INTO BUY(ADDRESS, AMOUNT, CODE, PRICE, ID)
+		VALUES(ADDRESS, _AMOUNT, _CODE, _PRICE*_AMOUNT, _ID);
+        
+    # PRODUCT 테이블에 제고량 수정 
+    UPDATE PRODUCT
+	SET
+		AMOUNT = AMOUNT - _AMOUNT
+	WHERE
+		CODE = _CODE;
+    
+    # CART 테이블에서 해당 장바구니 삭제 
+    DELETE FROM CART WHERE NUM = CART_NUM;
+END $$
+DELIMITER ;
+
+CALL BUY_CART(3, "경기도");
+# ACC001을 1개 구매
+# ACC001 제고가 7개로 변경 
+# 장바구니 3번이 삭제 
+
+
+
 
 
 

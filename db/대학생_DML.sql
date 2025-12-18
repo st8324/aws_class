56c72bee5911f137a73e8c61df1614422837bbe0
st8324 <stajun@naver.com>
2025-12-18 12:15:58 +0900

    자바 04일차 : 대학생 - DML

--- db/대학생_DML.sql
+++ db/대학생_DML.sql
@@ -22,6 +22,7 @@ PROFESSOR(PR_NUM, PR_NAME, PR_CONTACT, PR_RESIDENT, PR_YEAR, PR_MJ_CODE)
 VALUES("P2025160001", "홍교수", "111-1234-5678", "700101-5111111",
 2025, 160);
 
+/*
 INSERT INTO
 PROFESSOR(PR_NUM, PR_NAME, PR_CONTACT, PR_RESIDENT, PR_YEAR, PR_MJ_CODE)
 SELECT CONCAT("P", 2025, 160, LPAD(COUNT(*) + 1, 3, '0')),
@@ -29,7 +30,7 @@ SELECT CONCAT("P", 2025, 160, LPAD(COUNT(*) + 1, 3, '0')),
 2025, 160 FROM PROFESSOR 
 WHERE PR_YEAR = 2025 AND PR_MJ_CODE = 160;
 # WHERE PR_NUM LIKE CONCAT('_', 2025, 160,'%'); # 이 조건식으로 해도 결과 같음 
-
+*/
 # 교수 등록
 # 이름 : 김교수, 연락처 : 111-1111-1111, 주민번호 : 710101-5222222,
 # 임용연도 : 2025, 전공 : 컴퓨터공학과(160)
@@ -103,17 +104,98 @@ VALUES
 ('2025160003','160'),
 ('2025160004','160');
 
+# 과목 추가 
+# 컴퓨터 개론, 2학점, 2시간, 분류 : COM, 과목코드 : COM001
+# 프로그래밍 언어, 3학점, 3시간, 분류 : COM, 과목코드 : COM002
+# 알고리즘, 3학점, 4시간, 분류 : COM, 과모코드 : COM003
 
+# 확률과 통계, 3학점, 3시간, 분류 : MSC, 과목코드 : MSC001 
+# 미분과 적분, 4학점, 4시간, 분류 : MSC, 과목코드 : MSC002
+INSERT INTO SUBJECT(SJ_NAME, SJ_POINT, SJ_TIME, SJ_CODE)
+VALUES
+('컴퓨터 개론', 2, 2, 'COM001'),
+('프로그래밍 언어', 3, 3, 'COM002'),
+('알고리즘', 3, 4, 'COM003'),
+('확률과 통계', 3, 3, 'MSC001' ),
+('미분과 적분', 4, 4, 'MSC002');
 
+# 강의 등록
+# 컴퓨터 개론, 홍교수, 2025년 1학기, 1분반, 강의실 : 미디어관 101호, 정원 : 20
+# 강의 계획서 : "강의계획서입니다.", 시간 : "월1A,1B,2A,2B"
 
+# 프로그래밍 언어, 홍교수, 2025년 1학기, 1분반, 강의실 : 미디어관 201호, 정원 : 20
+# 강의 계획서 : "강의계획서입니다.", 시간 : "화1A,1B,2A,2B"
 
+# 프로그래밍 언어, 김교수, 2025년 1학기, 2분반, 강의실 : 미디어관 202호, 정원 : 20
+# 강의 계획서 : "강의계획서입니다.", 시간 : "화1A,1B,2A,2B"
+INSERT INTO LECTURE(lt_pr_num, lt_sj_code, lt_year, lt_semester, 
+lt_schedule, lt_room, lt_syllabus, lt_class, lt_max) 
+VALUES
+('P2025160001', 'COM001', 2025, 1, '월1A,1B,2A,2B', 
+'미디어관 101호', '강의계획서입니다.', 1, 20),
+('P2025160001', 'COM002', 2025, 1, '화1A,1B,2A,2B', 
+'미디어관 201호', '강의계획서입니다.', 1, 20),
+('P2025160002', 'COM002', 2025, 1, '화1A,1B,2A,2B', 
+'미디어관 202호', '강의계획서입니다.', 2, 20);
 
+# 알고리즘, 김교수, 2025년 1학기, 2분반, 강의실 : 미디어관 301호, 정원 : 20
+# 강의 계획서 : "강의계획서입니다.", 시간 : "목1A,1B,2A,2B,3A,3B"
 
+# 확률과 통계, 최교수, 2025년 1학기, 1분반, 강의실 : 하이관 101호, 정원 : 20
+# 강의 계획서 : "강의계획서입니다.", 시간 : "금1A,1B,2A,2B,3A,3B"
 
+# 미분과 적분, 장교수, 2025년 1학기, 1분반, 강의실 : 하이관 301호, 정원 : 20
+# 강의 계획서 : "강의계획서입니다.", 시간 : "목5A,5B,6A,6B,7A,7B"
 
+INSERT INTO LECTURE(lt_pr_num, lt_sj_code, lt_year, lt_semester, 
+lt_schedule, lt_room, lt_syllabus, lt_class, lt_max) 
+VALUES
+('P2025160002', 'COM003', 2025, 1, '목1A,1B,2A,2B,3A,3B', 
+'미디어관 301호', '강의계획서입니다.', 1, 20),
+('P2025456001', 'MSC001', 2025, 1, '금1A,1B,2A,2B,3A,3B', 
+'하이관 101호', '강의계획서입니다.', 1, 20),
+('P2025456002', 'MSC002', 2025, 1, '목5A,5B,6A,6B,7A,7B', 
+'하이관 301호', '강의계획서입니다.', 2, 20);
 
+# 홍길동(2025160001) 학생이 컴퓨터 개론, 프로그래밍언어(1), 알고리즘, 확률과 통계, 
+# 미분과 적분을 수강신청 함 
+INSERT INTO COURSE(CO_ST_NUM, CO_LT_NUM)
+VALUES
+('2025160001' , 1),('2025160001' , 2),('2025160001' , 4),
+('2025160001' , 5),('2025160001' , 6),
+# 고길동(2025160002) 학생이 컴퓨터 개론, 프로그래밍언어(2), 알고리즘, 확률과 통계, 
+# 미분과 적분을 수강신청 함 
+('2025160002' , 1),('2025160002' , 3),('2025160002' , 4),
+('2025160002' , 5),('2025160002' , 6),
+# 김길동(2025160003) 학생이 컴퓨터 개론, 프로그래밍언어(1) 을 수강신청 함 
+('2025160003' , 1),('2025160003' , 2),
+# 하니(2025160004) 학생이  프로그래밍언어(2), 알고리즘, 확률과 통계를 수강신청 함 
+('2025160004' , 3),('2025160004' , 4),('2025160004' , 5);
 
-
-
-
-
+UPDATE LECTURE 
+SET
+	LT_CURRENT = 
+      (SELECT COUNT(*) FROM COURSE WHERE CO_LT_NUM = 1)
+WHERE
+	LT_NUM = 1;
+UPDATE LECTURE 
+SET	LT_CURRENT = 
+      (SELECT COUNT(*) FROM COURSE WHERE CO_LT_NUM = 2)
+WHERE LT_NUM = 2;    
+UPDATE LECTURE 
+SET	LT_CURRENT = 
+      (SELECT COUNT(*) FROM COURSE WHERE CO_LT_NUM = 3)
+WHERE LT_NUM = 3;    
+UPDATE LECTURE 
+SET	LT_CURRENT = 
+      (SELECT COUNT(*) FROM COURSE WHERE CO_LT_NUM = 4)
+WHERE LT_NUM = 4;    
+UPDATE LECTURE 
+SET	LT_CURRENT = 
+      (SELECT COUNT(*) FROM COURSE WHERE CO_LT_NUM = 5)
+WHERE LT_NUM = 5;    
+UPDATE LECTURE 
+SET	LT_CURRENT = 
+      (SELECT COUNT(*) FROM COURSE WHERE CO_LT_NUM = 6)
+WHERE LT_NUM = 6;    
+COMMIT;

8fc1ea7c6f4fd7a3d9a01944b7c216634eb21229
st8324 <stajun@naver.com>
2026-01-26 17:17:35 +0900

    리액트 1일차 : boolean state 예제 - 박스 감추기

--- react/react1/src/App7.js
+++ react/react1/src/App7.js
@@ -1,18 +1,41 @@
+import { useState } from "react";
 
 /*
 - 접기 버튼을 누르면 글자가 더보기로 바뀌고, 
   내용이 감춰짐
 - 더보기 버튼을 누르면 글자가 접기로 바뀌고,
   내용이 보임	
+- state 변수를 boolean으로 선언
 */
 function App7(){
 
+	//isOpen이 true : 내용이 보임(접기 버튼)
+	//isOpen이 false: 내용이 안보임(더보기 버튼)
+	let [isOpen, setOpen] = useState(true);
+
+	//isOpen이 true이면 false로, false이면 true로. setOpen을 이용.
+	const btnClick = ()=>{ 
+		/*
+		if(isOpen){
+			setOpen(false);
+		}else{
+			setOpen(true);
+		}
+		*/
+		setOpen(!isOpen);
+	}
+
 	return (
 		<div>
-			<button>접기</button>
-			<div>
-				내용입니다.
-			</div>
+			<button onClick={btnClick}>{isOpen ? "접기" : "더보기"}</button>
+			{
+				isOpen ?
+				<div>
+					내용입니다.
+				</div>
+				:
+				null
+			}
 		</div>
 	);
 }

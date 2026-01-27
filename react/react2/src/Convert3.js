19ca7bec020a29acf01a2264cb1c2e1c8d92cd53
st8324 <stajun@naver.com>
2026-01-27 17:21:24 +0900

    리액트 2일차 : 단위 변환 예제

--- react/react2/src/Convert3.js
+++ react/react2/src/Convert3.js
@@ -6,21 +6,31 @@ import { useState } from "react";
 //숫자를 입력하면 입력한 숫자에 해당하는 초가 바로 출력되도록 구현
 function Convert3(){
 	let [amount, setAmount] = useState(0);
+	let [flag, setFlag] = useState(true);
+
+	const chageAmount = e => setAmount(e.target.value);
+	const btnClick = () =>{
+		setAmount(flag ? amount / 60 : amount * 60 );
+		setFlag(!flag);
+	}
 
 	return (
 		<div>
 			<div>
 				<input 
 					type="number" 
-					disabled={true} 
-					value={parseInt(amount/60)}/>
-				<button>변환</button>
+					onChange={chageAmount} 
+					disabled={flag} 
+					// flag일 때 parseInt(amount/60), !flat일 때 amount
+					value={flag ? parseInt(amount/60) : parseInt(amount)}/>
+				<button onClick={btnClick}>변환</button>
 			</div>
 			<input 
 				type="number" 
-				onChange={e=>setAmount(e.target.value)} 
-				disabled={false}
-				value={amount}/>
+				onChange={chageAmount} 
+				disabled={!flag}
+				// flag일 때 amount, !flag일 때 amount*60
+				value={flag ? parseInt(amount) : parseInt(amount * 60)}/>
 		</div>
 	);
 }

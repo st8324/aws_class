208a93a4bfce7f79d53d8e83429807c3cdce259c
st8324 <stajun@naver.com>
2026-01-29 13:53:09 +0900

    리액트 4일차 : todo 프로젝트 - 할일 목록 조회

--- react/todo/src/component/body/List.js
+++ react/todo/src/component/body/List.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 /* 
 비동기 통신으로 할일 목록 전체를 요청해서 가져온 후 화면에 출력
@@ -6,21 +6,47 @@ url : /api/v1/todos
 method : GET
 */
 function List(){
-
+	let [todos, setTodos] = useState([
+		{ num : 1, text : "점심", date : "2026-01-29", order : 1}, 
+		{ num : 2, text : "4교시", date : "2026-01-29", order : 2}, 
+	]);
 	useEffect(()=>{
 		//비동기 통신으로 할일 목록 전체를 요청해서 가져오는 함수 선언
+		const getTodos = async ()=>{
+			try{
+				const response = await fetch("/api/v1/todos",{
+					method : "GET",
+					// headers : {
+					// 	"Content-Type" : "application/json"
+					// },
+					// body : JSON.stringify({})
+				});
 
+				if(response.status == 200){
+					const result = await response.json();
+					console.log(result);
+				}
+			}catch(e){
+				console.error(e);
+			}
+		}
 		//함수 호출
-
+		getTodos();
 	}, []);
 	return(
 		<div>
 			<h1>할일 목록</h1>
 			<ul>
-				<li>
-					<span>2026-01-29</span> 
-					<span>점심</span>
-				</li>
+				{
+					todos.map(todo=>{
+						return (
+							<li key={todo.num}>
+								<span>{todo.date}</span>
+								<span>{todo.text}</span>
+							</li>
+						)
+					})
+				}
 			</ul>
 		</div>
 	)

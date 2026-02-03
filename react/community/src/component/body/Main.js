4dc96de003511707c52bdc09cd742b6090564f58
st8324 <stajun@naver.com>
2026-02-03 14:22:35 +0900

    리액트 7일차 : 로그인 여부에 따라 메뉴 변경

--- react/community/src/component/body/Main.js
+++ react/community/src/component/body/Main.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { authFetch } from "../../api/authFech";
 
 function Main(){
@@ -35,11 +36,71 @@ function Main(){
 			console.error(e);
 		}
 	}
+	const LIST = [
+		{age : 20, name : '홍길동'},
+		{age : 30, name : '고길동'},
+		{age : 40, name : '홍둘리'},
+	];
+	const [list, setList] = useState(LIST);
+
+	/* 
+	filter
+	- 배열에서 조건에 맞는 항목들만 거름
+	- 배열 항목 개수는 변경
+	- 배열 항목 구조는 안 바뀜
+	- true/false를 반환하는 함수가 필요
+	  => false가되면 거름
+	map
+	- 베열에서 항목들을 가져와 변경하여 배열을 만듬
+	- 배열 항목 개수 변경 없음
+	- 배열 항목 구조 바뀜 
+	- 어떤 값(객체, 숫자, 문자열)을 반환하는 함수가 필요
+	  => 어떤 값들을 이용해서 배열을 만듬
+	*/
+	const filter1 = ()=>{
+		const filterList = LIST.filter(item=>{
+			return item.age >= 30;
+		});
+		setList(filterList);
+	}
+	const filter2 = ()=>{
+		const filterList = LIST.filter(item=>{
+			return item.name.startsWith("홍");
+		});
+		setList(filterList);
+	}
+	const filter3 = ()=>{
+		const filterList = LIST.filter(item=>{
+			return item.name.endsWith("길동");
+		});
+		setList(filterList);
+	}
+	const filter4 = (checkCallback)=>{
+		const filterList = LIST.filter(item=>{
+			return checkCallback(item);
+		});
+		setList(filterList);
+	}
 	return(
 		<div>
 			<h1>메인</h1>
 			<button onClick={fetchHandler}>인증 테스트</button>
 			<button onClick={fetchHandler2}>회원 정보 테스트</button>
+			<div>
+				<button onClick={filter1}>30살 이상만</button>
+				<button onClick={filter2}>성이 "홍"인 사람들만</button>
+				<button onClick={filter3}>이름이 "길동"인 사람들만</button>
+			</div>
+			<div>
+				<button onClick={()=>filter4((item)=>item.age >= 30)}>30살 이상만</button>
+				<button onClick={()=>filter4((item)=>item.name.startsWith("홍"))}>성이 "홍"인 사람들만</button>
+				<button onClick={()=>filter4((item)=>item.name.endsWith("길동"))}>이름이 "길동"인 사람들만</button>
+			</div>
+			<ul>
+				{!list ? null : list.map(item=>{
+					return <li>{item.age}, {item.name}</li>
+				})}
+			</ul>
 		</div>
 	)
 }
--- react/community/src/component/nav/Navbar.js
+++ react/community/src/component/nav/Navbar.js
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css"
 import { useAuth } from "../../AuthContext";
+import { useState } from "react";
 
 
 
 const NAV_ITEMS = [
-	{ label : '메인', path : '/'},
-	{ label : '회원가입', path : '/signup'},
-	{ label : '로그인', path : '/login'},
+	{ label : '메인', path : '/', auth : 'any'},
+	{ label : '회원가입', path : '/signup', auth : 'guest'},
+	{ label : '로그인', path : '/login', auth : 'guest'},
+	{ label : '마이페이지', path : '/mypage', auth : 'user'}
 ];
 
 
 function Navbar(){
 	const {user} = useAuth();
+
+	//로그인 상태에 따라 보여지는 메뉴를 다르게 설정
+	const navItems = NAV_ITEMS.filter(item=>{
+		if(item.auth === 'any')	return true;
+		if(item.auth === 'guest') return !user;
+		if(item.auth === 'user') return user;
+	});
+
 	return (
 		<nav className="navbar">
 			<ul className="menu-list">
 				{
-				NAV_ITEMS.map(item=>{
+				navItems.map(item=>{
 					return (
 						<li className="menu-item" key={item.path}>
 							<Link className="menu-link" to={item.path}>{item.label}</Link>

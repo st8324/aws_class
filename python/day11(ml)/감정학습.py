97c7ad3031718ae605d28b97d26bf630dba52a0f
st8324 <stajun@naver.com>
2026-03-23 18:14:46 +0900

    파이썬 11일차 : ml - 텍스트 마이닝 라이브러리 분리 예제

--- python/day11(ml)/감정학습.py
+++ python/day11(ml)/감정학습.py
@@ -44,8 +44,28 @@ def train_and_save_model(dataset_x, dataset_y):
 	}
 	jl.dump(model_data, 'model.pkl')
 
+def predict(text):
+	# 모델과 벡터라이저를 불러옴
+	mode_data = jl.load('model.pkl')
+	model , vectorizer = mode_data['model'], mode_data['vectorizer']
+
+	# 형태소가 처리된 문자열로 변환
+	cleaned_text = text_preprocessing(text)
+
+	# 문장을 벡터화 
+	vector_text = vectorizer.transform([cleaned_text])
+
+	# 예측
+	predict = model.predict(vector_text)
+	# 예측 결과를 반환(0 또는 1)
+	return predict[0]
+
 if __name__ == '__main__':
 	# print(text_preprocessing("오늘은 날이 너무 너무 좋습니다."))
-	df = pd.read_csv(r'sample.csv')
-	train_and_save_model(df['sentence'], df['label'])
-	pass
+	# 테스트 할 때 sample.csv를 인식하게 하기 위해 cd day11(ml) 명령어 입력 후 실행
+	# df = pd.read_csv(r'sample.csv')
+	# train_and_save_model(df['sentence'], df['label'])
+	print(predict('오늘은 날이 너무 너무 화창합니다.'))
+	print(predict('날씨가 화창한데 자격증 시험에 떨어졌습니다.'))
+	print(predict('상사한테 혼났습니다.'))
+	

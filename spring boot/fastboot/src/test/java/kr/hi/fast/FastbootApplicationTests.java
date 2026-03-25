9a297ba615f897bfd4c059be87111164893b839f
st8324 <stajun@naver.com>
2026-03-25 14:22:55 +0900

    파이썬 12일차 : ml - fast api 연동 스프링 프로젝트 생성 및 작업중

---
+++ spring boot/fastboot/src/test/java/kr/hi/fast/FastbootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hi.fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

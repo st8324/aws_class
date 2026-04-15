ab25b76b0883f8dd2829036c2283f97d1a13e412
st8324 <stajun@naver.com>
2026-04-15 10:21:24 +0900

    OpenAI 02일차 : 리액트 프로젝트 생성 및 라우터 적용

---
+++ openai/front/src/App.js
@@ -0,0 +1,17 @@
+import List from "./List";
+import Main from "./Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Main/>} />
+        <Route path="/list" element={<List/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ openai/front/src/List.jsx
@@ -0,0 +1,10 @@
+
+function List(){
+	return(
+		<div>
+			<h1>기능들 모음</h1>
+		</div>
+	)
+}
+
+export default List
